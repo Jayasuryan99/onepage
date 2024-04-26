0af61ce8c28724e05660adef92b8d65a499d59e9
jayasuryan99 <jayasuryan99@gmail.com>
2024-04-26 09:12:00 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import List from './component/List';
+import Delivery from './Assests/delivery.png'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='final'> 
+
+    <ul>
+      <li> <a class='active' href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a class="about" href="#about">About</a></li>
+      <input className='search' type='search' placeholder='search here'/>
+    </ul>
+
+    <div className='para'> 
+    <h1> Hunger is a <br/> good cook. </h1>
+    </div> 
+
+    <div className='image'> 
+    <img src={Delivery} alt="" />
+    </div>
+     
+    <div>
+    <p className='continuous'>  To live a full life,you have to fill your stomach first.<br/>
+      Quality food is thewmost important thing in our life.</p>
+    </div>
+
+    <div className='list'> 
+        <List />
+     </div>
+  
+  <div className='lastpara'> 
+     </div>
+     <button className="primary-button"> Bookings Now </button> 
+     <button className="primary-button1"> See Our Menu </button>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/component/List.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div className='trending'>
+      <div className='trending1'>
+        <h1> Trending Item </h1>
+        <p> -25% off</p>
+        <p> Enjoy our specially prepared </p>
+        <p> Items for you </p>
+     </div>   
+     <div className='trending2'>
+        <h1> Dessert Menu </h1>
+        <p> Delivering best ingredients
+        for our trust food lovers</p>
+     </div>     
+     <div className='trending3'>
+        <h1> Nearest Restaurant </h1>
+        <p> Each kitchen works with its own delivery area <br/>
+            to deliver food to you as soon as possible </p>
+     </div>    
+    </div>
+  )
+}
+
+export default List
